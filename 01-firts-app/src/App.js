aace9c11f6c34385157d85a5c0b623f91e6a4104
josmig <pantigosomiguel59@gmail.com>
2020-07-14 18:52:01 -0500

    uso de los props para pasar informacion entre los componentes - props basicos

--- 01-firts-app/src/App.js
+++ 01-firts-app/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HolaMundo, {Adios} from './components/HolaMundo';
+/* import HolaMundo, {Adios} from './components/HolaMundo';
 import Persona from './components/Persona';
-
+ */
+import Saludar from './components/Saludar';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HolaMundo/>
-        <Adios/>
-        <Persona/>
+        {/* <HolaMundo/>
+        <Adios/>        <Persona/> */}
+        <Saludar name="Miguel Pantigoso" edad="22" />
       </header>
     </div>
   );
