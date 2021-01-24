d9f2cfe479f01f43756b9969d85d10cd77764186
josmig <pantigosomiguel59@gmail.com>
2021-01-24 14:39:06 -0500

    creacion de componentes

---
+++ 03-react-basics/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';    // #1 debemos importar siempre la libreria para usar todo sobre React
+
+function Header(){            // #2 creación de una función + return ahi se coloca lo que se ve en pantalla
+    return(
+        <h1>Desde el header</h1>
+    )
+}
+
+export default Header;
