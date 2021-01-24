702af2607106f91d51450a8081357ff2a4d955b0
josmig <pantigosomiguel59@gmail.com>
2021-01-24 18:44:49 -0500

    eventos en React

--- 03-react-basics/src/components/Producto.js
+++ 03-react-basics/src/components/Producto.js
@@ -4,9 +4,20 @@ const Producto = ({producto}) => {
     
     const {nombre , precio, id} = producto
     
+
+    //add producto al carrito
+    const seleccionarProducto = id => {
+        console.log('comprando...', id);
+    }
     return (     
         <div>
             <h2>El producto {nombre} tiene un precio de {precio}</h2>
+            <button 
+                type="button"
+                onClick={ ()=> seleccionarProducto(id)}
+
+            >Comprar</button>
+
         </div>
     );
 }
